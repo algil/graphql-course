af27d3d872e9ba71bc7ae8e186c11ba2a3cbe753
Antonio Gil <antonioluisgil@gmail.com>
2019-04-19 22:34:10 +0200

    Add graphql-yoga server with basic queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,2 +1,36 @@
-import { data } from './demo';
-console.log('Hello GraphQL', data.name);
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query';
+    },
+    name() {
+      return 'Antonio Gil';
+    },
+    location() {
+      return 'Málaga (Spain)';
+    },
+    bio() {
+      return 'Music lover and guitar player on Zoonámbulos band';
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('Server is running in http://localhost:4000');
+});
