13f87f09eb5181d2341666235b00c58dceca135f
Antonio Gil <antonioluisgil@gmail.com>
2019-04-21 01:51:37 +0200

    Add relations in types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,23 +1,46 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+const users = [
+  {
+    id: '1',
+    name: 'Antonio',
+    email: 'antonio@example.com',
+    age: 36
+  },
+  {
+    id: '2',
+    name: 'Williams',
+    email: 'williams@example.com'
+  },
+  {
+    id: '3',
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 39
+  }
+];
+
 const posts = [
   {
     id: '1',
     title: 'Megalodon',
     body: 'A very big shark in the sea',
-    published: true
+    published: true,
+    author: '1'
   },
   {
     title: '2',
     title: 'Aquaman',
     body: 'The king of the sea and oceans',
-    published: true
+    published: true,
+    author: '1'
   },
   {
     id: '3',
     title: 'Avengers Infinity War',
     body: 'The best Marvel heroes fighting against the evil Thanos',
-    published: false
+    published: false,
+    author: '3'
   }
 ];
 
@@ -25,6 +48,7 @@ const typeDefs = `
   type Query {
     me: User!
     post: Post!
+    users: [User!]!
     posts(query: String): [Post!]!
   }
   type User {
@@ -32,12 +56,14 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
   type Post {
     id: ID!
     title: String!
     body: String!
     published: Boolean!
+    author: User!
   }
 `;
 
@@ -58,6 +84,9 @@ const resolvers = {
         published: true
       };
     },
+    users() {
+      return users;
+    },
     posts(parent, args) {
       if (!args.query) {
         return posts;
@@ -70,6 +99,16 @@ const resolvers = {
           body.toLowerCase().includes(query)
         );
       });
+    }
+  },
+  Post: {
+    author(parent) {
+      return users.find(user => user.id === parent.author);
+    }
+  },
+  User: {
+    posts(parent) {
+      return posts.filter(post => post.author === parent.id);
     }
   }
 };
