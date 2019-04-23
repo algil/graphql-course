9d9334fca482f21ce23e06fa01f567613e241730
Antonio Gil <antonioluisgil@gmail.com>
2019-04-23 21:34:31 +0200

    Add subcription for create post mutation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,12 +1,15 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './db';
 import resolvers from './resolvers';
+
+const pubSub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   context: {
-    db
+    db,
+    pubSub
   }
 });
 
